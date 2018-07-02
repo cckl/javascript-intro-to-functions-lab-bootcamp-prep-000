e8f3bb0b73f9b46ee5407bd1cd58455ed6c4b6dc
Christina L <clingc7@gmail.com>
2018-07-02 03:04:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log("HELLO")
+}
+
+function logWhisper(string) {
+  console.log("hello")
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too." 
+  }
+}
+
